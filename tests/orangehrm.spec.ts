97e38e98a76c3736d494605c96e31196c34e3556
skbnawaz <skbnawaz3@gmail.com>
2026-06-08 18:36:34 +0530

    first commit

---
+++ tests/orangehrm.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "@playwright/test";
+
+test("orangeHrm login", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com");
+  await page.locator('[data-test="username"]').fill("standard_user");
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="title"]')).toBeVisible();
+});
+//this line is added
